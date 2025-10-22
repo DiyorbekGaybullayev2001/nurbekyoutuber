44f324e42e5028807a896dc2b9818ff1ee937c7e
Diyorbek G'aybullayev <gaybullayevdiyorbek2001@gmail.com>
2025-10-22 21:27:03 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <>
       <Toaster />
+      <Analytics />
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
